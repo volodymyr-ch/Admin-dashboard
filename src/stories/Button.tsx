8bf59fc6e9f872e10f8f9ad01c874c1b57dddc2d
Volodymyr Cherkavskyi <vcherkavskyi@n-ix.com>
2024-02-24 22:33:26 +0200

    feat: add storybook to build isolated components

---
+++ src/stories/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './button.css';
+
+type ButtonProps = {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large';
+  label: string;
+  onClick?: () => void;
+};
+
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  onClick = () => {},
+}: ButtonProps) => {
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' '
+      )}
+      style={{ backgroundColor }}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
